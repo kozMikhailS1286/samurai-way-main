93afa77bb093d1edffae8c0ac0bd36a0fcca209b
Mikhail <koz.mikhail.s@yandex.ru>
2022-11-11 23:58:32 +0300

    Video 31

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Post from './Post/Post';
 import s from './MyPosts.module.css'
 import { PostType } from './../../../redux/state';
@@ -10,15 +11,22 @@ const MyPosts = (props: MyPostType) => {
 
     let postsElements = props.posts.map(p => <Post id={p.id} message={p.message} likesCount={p.likesCount} /> )
 
+    let newPostElement: any = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    }
+
     return (
        <div className={s.postsBlock}>
            <h3> My posts </h3>
            <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={ newPostElement }></textarea>
                 </div>
                 <div>    
-                    <button> Add post </button>
+                    <button onClick={ addPost }> Add post </button>
                 </div>
             </div>
 
