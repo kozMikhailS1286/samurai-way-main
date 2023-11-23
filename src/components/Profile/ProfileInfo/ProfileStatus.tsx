89c47cc07deb12f0e79cb1465a2e20005ddf763a
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2023-11-23 17:19:30 +0300

    Video 72 finish

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -28,11 +28,11 @@ class ProfileStatus extends React.Component<ProfileStatusPropsType>{
             <div>
                 {!this.state.editMode &&
                     <div>
-                        <span onDoubleClick={this.activateEditMode.bind(this)}> {this.props.status} </span>
+                        <span onDoubleClick={this.activateEditMode}> {this.props.status} </span>
                     </div>}
                 {this.state.editMode &&
                     <div>
-                        <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
+                        <input autoFocus={true} onBlur={this.deactivateEditMode} value={this.props.status}/>
                     </div>}
             </div>)
     }
