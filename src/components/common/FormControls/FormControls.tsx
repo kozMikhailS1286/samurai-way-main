23bf290aa552c86d8022baaf0b3dc797f94ac6fa
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2024-01-11 22:17:45 +0300

    Video 77 2/3

--- src/components/common/FormControls/FormControls.tsx
+++ src/components/common/FormControls/FormControls.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import s from './FormControls.module.css'
 
-export const Textarea = (props: any) => {
+export const Textarea = ({input, meta, ...props}: any) => {
+    const hasError = meta.touched && meta.error;
     return (
-        <div>
-            <textarea {...props.input} />
+        <div className={s.formControl + " " + (hasError ? s.error : "")}>
+            <textarea {...input}{...props} />
+            <div>
+                {hasError && <span> {meta.error} </span>}
+            </div>
         </div>
     )
 }
